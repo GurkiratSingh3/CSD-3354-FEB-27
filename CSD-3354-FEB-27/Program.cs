7a717ca61a3d990668d53eafc3e7ac965b52062a
visualstudio <visualstudio@122-PC29>
2019-02-27 09:43:05 -0500

    second

--- CSD-3354-FEB-27/Program.cs
+++ CSD-3354-FEB-27/Program.cs
@@ -15,4 +15,22 @@ namespace CSD_3354_FEB_27
             orderProcessor.Process(order);
         }
     }
+    public class OrderProcessor
+    {
+        private readonly ShippingCalculator _shippingCalculator;
+            public OrderProcessor()
+        {
+            _shippingCalculator = new ShippingCalculator();
+        }
+        public void Proccess(Order order)
+        {
+            if (order.IsShipped)
+                throw new InvalidIperationException("This order is already processed.");
+            order.Shipment = new Shipment
+            {
+                Cost = _shippingCalculator.CalculateShipping(order),
+                ShippingDate = DateTime.Today.AddDays(1);
+            }
+        }
+    }
 }
